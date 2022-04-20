b14d6d608a39406a3e92d0c13b50e531b3fee708
DeepBlue97 <945048933@qq.com>
2022-04-20 16:05:05 +0800

    maskrcnn

--- infer_faster_processor_mask_rcnn.py
+++ infer_faster_processor_mask_rcnn.py
@@ -9,11 +9,13 @@ infer more faster then "from mmdeploy.apis import inference_model"
 """
 
 # FP16  successful with: win10 2060s torch182 mmcv146 mmdet2.22.0-peter_custom
-model_cfg = r'E:\workspace\win10\lab_mmdeploy_v4\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
-deploy_cfg = r'E:\workspace\win10\lab_mmdeploy_v4\mmdeploy\configs\mmdet\instance-seg\instance-seg_tensorrt_dynamic-320x320-1344x1344.py'
+# model_cfg = r'E:\workspace\win10\lab_mmdeploy_v4\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
+# deploy_cfg = r'E:\workspace\win10\lab_mmdeploy_v4\mmdeploy\configs\mmdet\instance-seg\instance-seg_tensorrt_dynamic-320x320-1344x1344.py'
+model_cfg = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
+deploy_cfg = r'E:\workspace\win10\lab_mmdeploy\mmdeploy\configs\mmdet\instance-seg\instance-seg_tensorrt_dynamic-320x320-1344x1344.py'
 device = 'cuda:0'
-backend_files = [r'E:\workspace\win10\lab_mmdeploy_v4\work_dir_mask_rcnn_fp32\end2end.engine']
-img = r'E:\workspace\win10\lab_mmdeploy_v4\el.png'
+backend_files = [r'E:\workspace\win10\lab_mmdeploy\work_dir_mask_rcnn_fp16\end2end.engine']
+img = r'E:\workspace\win10\lab_mmdeploy\el.png'
 
 deploy_cfg, model_cfg = load_config(deploy_cfg, model_cfg)
 
@@ -28,11 +30,11 @@ with torch.no_grad():
 
     t1 = time.time()
     img_num = 100
-    print('start infer')
+    print('start infer ...')
     for i in range(img_num):
 
         result = task_processor.run_inference(model, model_inputs)
     print(img_num / (time.time()-t1), ': img/s')
 
 # print(result)
-print('finish infer')
+print('finish infer.')
