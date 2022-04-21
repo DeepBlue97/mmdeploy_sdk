1f62ace8c066e97dd1ad65983264c74c6963a55d
DeepBlue97 <945048933@qq.com>
2022-04-21 10:51:42 +0800

    ops

--- ops_pytorch_print.py
+++ ops_pytorch_print.py
@@ -1,9 +1,15 @@
-import imp
 from mmdet.apis import init_detector
 
 
 config = r'D:\workspace\fork\mmdetection\configs\yolox/yolox_nano_8x8_300e_coco_person_only.py'
 checkpoint = r'D:\workspace\fork\mmdetection\checkpoints\best_bbox_mAP_epoch_299.pth'
+
+config = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
+checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\mask_rcnn_epoch_12.pth'
+
+config = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\htc\htc_r50_fpn_1x_coco.py'
+checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\epoch_1_htc.pth'
+
 
 model = init_detector(config, checkpoint=None, device='cuda:0', cfg_options=None)
 
