b7108106058f075b7ac23925eb9ffc70ed43f4c3
DeepBlue97 <945048933@qq.com>
2022-04-21 15:19:22 +0800

    net print

--- ops_pytorch_print.py
+++ ops_pytorch_print.py
@@ -2,11 +2,11 @@ from mmdet.apis import init_detector
 import torch
 
 
-config = r'D:\workspace\fork\mmdetection\configs\yolox/yolox_nano_8x8_300e_coco_person_only.py'
+config = r'E:\workspace\win10\fork\mmdetection\configs\yolox\yolox_nano_8x8_300e_coco_person_only.py'
 checkpoint = r'D:\workspace\fork\mmdetection\checkpoints\best_bbox_mAP_epoch_299.pth'
 
-config = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
-checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\mask_rcnn_epoch_12.pth'
+# config = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py'
+# checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\mask_rcnn_epoch_12.pth'
 
 # config = r'E:\workspace\win10\lab_mmdeploy\mmdetection\configs\htc\htc_r50_fpn_1x_coco.py'
 # checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\epoch_1_htc.pth'
@@ -14,7 +14,7 @@ checkpoint = r'E:\workspace\win10\lab_mmdeploy\mmdetection\checkpoints\mask_rcnn
 
 model = init_detector(config, checkpoint=None, device='cuda:0', cfg_options=None)
 
-# print(model)
+print(model)
 
 dummy_input = torch.rand((1, 3, 640, 640))
 
--- train_yolox.md
+++ train_yolox.md
@@ -1,5 +1,5 @@
 python .\tools\train.py `
-E:\workspace\win10\lab_mmdeploy\mmdetection\configs\mask_rcnn\mask_rcnn_r50_fpn_1x_coco.py `
+E:\workspace\win10\lab_mmdeploy\mmdetection\configs\yolox\yolox_nano_8x8_300e_coco.py `
 --cfg-options data.samples_per_gpu=1 data.workers_per_gpu=1 `
 
 
